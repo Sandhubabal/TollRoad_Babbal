85cc00b30e04d231a3f91c485aca83a1f0d5e903
Sandhubabal99@icloud.com <Sandhubabal99@icloud.com>
2021-09-02 14:31:08 +0300

    Add project files.

---
+++ TollRoad_Babbal/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TollRoad_Babbal.Models;
+
+namespace TollRoad_Babbal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<TollPrice> TollPrices { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ TollRoad_Babbal/Models/TollPrice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TollRoad_Babbal.Models
+{
+    public class TollPrice
+    {
+        public int ID { get; set; }
+        public decimal Charges { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
